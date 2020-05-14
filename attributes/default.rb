486a7751652f1132598edfa93d162715fdbe5e6b
Jim Quilty <jquilty@chef.io>
2020-05-14 15:11:34 -0400

    initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['audit']['reporter'] = 'chef-automate'
+default['audit']['profiles']['tmp_compliance_profile'] = {
+  'url': 'https://github.com/jimquilty/httpd-checks',
+}
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,1 @@
-#
-# Cookbook:: my_audit
-# Recipe:: default
-#
-# Copyright:: 2020, The Authors, All Rights Reserved.
+include_recipe 'audit::default'
